4cd183390b675532ff7f89b56e29cd7608c6484e
Hoang Dung <dungnguyen322003@gmail.com>
2022-09-05 18:51:07 +0800

    add ieNoOpen

--- myApp.js
+++ myApp.js
@@ -5,10 +5,14 @@ const helmet = require('helmet');
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
+
+// helmet
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get('/', function (request, response) {
