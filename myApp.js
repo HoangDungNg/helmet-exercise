7dd987ffe3931a20d0c40d639eb08b863df47cbf
Hoang Dung <dungnguyen322003@gmail.com>
2022-09-05 18:30:52 +0800

    add xssFilter

--- myApp.js
+++ myApp.js
@@ -7,6 +7,7 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.xssFilter());
 app.disable('strict-transport-security');
 app.use('/_api', api);
 app.get('/', function (request, response) {
