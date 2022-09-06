78792ce056f19048448d62e67a0186745783837a
Hoang Dung <dungnguyen322003@gmail.com>
2022-09-06 13:27:01 +0800

    change in scriptSrc

--- myApp.js
+++ myApp.js
@@ -24,7 +24,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", 'trusted-cdn.com'],
+      scriptSrc: ["'self'", "trusted-cdn.com"],
     },
   })
 );
